8b134d0d7601bf96b26951f14100db0cf0fd09a4
SajjadHossain0 <102672264+SajjadHossain0@users.noreply.github.com>
2022-10-31 14:26:06 +0000

    Add files via upload

---
+++ main.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+            String answer;
+       do {
+           Scanner input = new Scanner(System.in);
+           System.out.print("Enter rock/paper/scissor : ");
+           String player = input.next();
+
+           String[] com = {"rock", "paper", "scissor"};
+           Random rand = new Random();
+           int comp = rand.nextInt(com.length);
+           String computer = com[comp];
+
+           System.out.println("you choose : "+player + "\nComputer : "+computer);
+           if (player.equals("rock") && computer.equals("rock"))
+               System.out.println("Tie...");
+           else if (player.equals("paper") && computer.equals("paper"))
+               System.out.println("Tie...");
+           else if (player.equals("scissor") && computer.equals("scissor"))
+               System.out.println("Tie...");
+
+           else if (player.equals("rock") && computer.equals("scissor"))
+               System.out.println("\nCongratulation! You win...\n");
+           else if (player.equals("paper") && computer.equals("rock"))
+               System.out.println("\nCongratulation! You win...\n");
+           else
+               System.out.println("\n\tSorry! You loose...\n");
+
+
+           System.out.print("press  0  for play again : ");
+           answer = input.next();
+       }
+       while (answer.equals("0"));
+
+    }
+}
