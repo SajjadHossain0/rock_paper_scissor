124061c8c62229eae014d955fca46a33339b311b
SajjadHossain0 <102672264+SajjadHossain0@users.noreply.github.com>
2022-11-03 15:32:10 +0000

    Update main.java

--- main.java
+++ main.java
@@ -2,37 +2,39 @@ import java.util.Random;
 import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
-            String answer;
-       do {
-           Scanner input = new Scanner(System.in);
-           System.out.print("Enter rock/paper/scissor : ");
-           String player = input.next();
+        String answer;
+        do {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter rock/paper/scissor : ");
+            String player = input.next();
 
-           String[] com = {"rock", "paper", "scissor"};
-           Random rand = new Random();
-           int comp = rand.nextInt(com.length);
-           String computer = com[comp];
+            String[] com = {"rock", "paper", "scissor"};
+            Random rand = new Random();
+            int comp = rand.nextInt(com.length);
+            String computer = com[comp];
 
-           System.out.println("you choose : "+player + "\nComputer : "+computer);
-           if (player.equals("rock") && computer.equals("rock"))
-               System.out.println("\nOh no! It's Tie...\n");
-           else if (player.equals("paper") && computer.equals("paper"))
-               System.out.println("\nOh no! It's Tie...\n");
-           else if (player.equals("scissor") && computer.equals("scissor"))
-               System.out.println("\nOh no! It's Tie...\n");
+            System.out.println("you choose : "+player + "\nComputer : "+computer);
+            if (player.equals("rock") && computer.equals("rock"))
+                System.out.println("\nOh no! It's Tie...\n");
+            else if (player.equals("paper") && computer.equals("paper"))
+                System.out.println("\nOh no! It's Tie...\n");
+            else if (player.equals("scissor") && computer.equals("scissor"))
+                System.out.println("\nOh no! It's Tie...\n");
 
-           else if (player.equals("rock") && computer.equals("scissor"))
-               System.out.println("\nCongratulation! You win...\n");
-           else if (player.equals("paper") && computer.equals("rock"))
-               System.out.println("\nCongratulation! You win...\n");
-           else
-               System.out.println("\n\tSorry! You loose...\n");
+            else if (player.equals("rock") && computer.equals("scissor"))
+                System.out.println("\nCongratulation! You win...\n");
+            else if (player.equals("paper") && computer.equals("rock"))
+                System.out.println("\nCongratulation! You win...\n");
+            else if (player.equals("scissor") && computer.equals("paper"))
+                System.out.println("\nCongratulation! You win...\n");
+            else
+                System.out.println("\n\tSorry! You loose...\n");
 
 
-           System.out.print("press  0  for play again : ");
-           answer = input.next();
-       }
-       while (answer.equals("0"));
+            System.out.print("press  0  for play again : ");
+            answer = input.next();
+        }
+        while (answer.equals("0"));
 
     }
 }
